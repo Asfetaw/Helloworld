2853f7ac7d7f30d68e816c0ffa1b4faab594dc58
Asfetaw <asfetaw@hotmail.com>
2021-05-09 12:43:39 -0700

    updating setupfile

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="test_pgk_astfetaw_abera", # Replace with your own username
+    name="HelloWorld", 
     version="0.0.1",
     author="Asfetaw Abera",
     author_email="asfetu@gmail.com",
